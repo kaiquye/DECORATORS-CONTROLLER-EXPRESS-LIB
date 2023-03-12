6693adcade78f1b0d75785bb145b96d8d1d9b55c
@Alnkle621 <kaiquemendesilva82@gmail.com>
2023-03-12 19:07:57 -0300

    :chore: add Controller

--- lib/controllerDecoreitor.service.ts
+++ lib/controllerDecoreitor.service.ts
@@ -27,3 +27,12 @@ export abstract class ControllerBase {
   public _routers = [];
   public _middlewaresDto = [];
 }
+
+export const Controller = (name?: string): Function => {
+  return (target: any, key: string): void => {
+    const _routersController = target.prototype._routers;
+    const _dtosController = target.prototype._middlewaresDto;
+
+    console.log(_routersController, _dtosController);
+  };
+};
